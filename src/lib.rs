45c667fffbb75fbf7991b31e79d4a9103f5d6ef9
arcnmx <arcnmx@users.noreply.github.com>
2021-08-04 20:40:08 -0700

    fix doc example whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,11 @@
 //! for mut display in Display::enumerate() {
 //!     display.update_capabilities().unwrap();
 //!     println!("{:?} {}: {:?} {:?}",
-//!			display.info.backend, display.info.id,
-//!			display.info.manufacturer_id, display.info.model_name
-//!		);
+//!         display.info.backend, display.info.id,
+//!         display.info.manufacturer_id, display.info.model_name
+//!     );
 //!     if let Some(feature) = display.info.mccs_database.get(0xdf) {
-//!			let value = display.handle.get_vcp_feature(feature.code).unwrap();
+//!         let value = display.handle.get_vcp_feature(feature.code).unwrap();
 //!         println!("{}: {:?}", feature.name.as_ref().unwrap(), value);
 //!     }
 //! }
